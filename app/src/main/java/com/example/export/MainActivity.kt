6f7b408b3aefd02cba72993cb4a3537cc75b4e0b
Raniaaaaa8 <hr_bouguesri@esi.dz>
2021-07-06 19:22:10 +0100

    search multiple ayat and display in textview

--- app/src/main/java/com/example/export/MainActivity.kt
+++ app/src/main/java/com/example/export/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.export
 
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -13,6 +14,7 @@ import java.nio.charset.Charset
 
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -26,14 +28,43 @@ class MainActivity : AppCompatActivity() {
         getIdRacine.setOnClickListener {
             racine=getRacine.text.toString()
             var ele=ReadDatafromcsvRacine_des_mots_arabe(racine)
-            if (ele!=null) displayIdRacine.setText(ele.idRacine)
+            if (ele!=null) {
+                displayIdRacine.setText(ele.idRacine)
+                //val idaya=ReadDatafromcsvMot_des_racines(ele.idRacine)
 
-            /*var IdRacines: Array<ElementscsvMotsdesRacines>? =null
-            val idaya=ReadDatafromcsvMot_des_racines(ele.idRacine)
-            IdRacines?.set(0,idaya )
-            //afficher les aya
+                //afficher les aya
+                var ayat= ReadDatafromcsvMot_des_racines(ele.idRacine)//mutableListOf(idaya)// = arrayOf<ElementscsvMotsdesRacines>()
+                //IdRacines.add(idaya)
+                var str= ayat?.get(0)?.englishWord
+                if(str==null) str=""
+                Log.d("array aya",str)
+                str=""
+                var f:String?
+                if (ayat != null) {
+                    var cpt=1
+                    for ( i in ayat ){
+                        if (cpt==ayat.size) break
+                        f=ReadDatafromcsvAya(ayat?.get(cpt)?.ID_Aya).Text_AR
+                        if (f!=null) str=str+"  ***  "+f
 
-            ReadDatafromcsvAya(IdRacines?.get(0)?.ID_Aya)*/
+                        Log.d("cpt", cpt.toString())
+                        var str2=ayat?.get(cpt)?.ID_Aya
+                        if (str2!=null)
+                        Log.d("ayat",str2 )
+                        if (f != null) {
+                            Log.d("f",f)
+                        }
+                        cpt++
+                    }
+
+                }
+                /*val ayastr=ReadDatafromcsvAya(ayat?.get(0)?.ID_Aya)*/
+                if (str!=null) aya.setText(str)
+                else aya.setText("Not found")
+            }
+            else displayIdRacine.setText("Not found")
+
+
         }
 
 
@@ -71,6 +102,9 @@ class MainActivity : AppCompatActivity() {
                 }
                 //line= reader.readLine()
                 //if (line==null) break
+                else if(tokens[1]=="99"){
+                    break
+                }
             }
         } catch (e: IOException) {
             e.printStackTrace()
@@ -106,6 +140,9 @@ class MainActivity : AppCompatActivity() {
                     tub.NBLettre=tokens[7]
                     break
                 }
+                else if(tokens[0]=="1799"){
+                    break
+                }
                 //line= reader.readLine()
                 //if (line==null) break
             }
@@ -118,8 +155,12 @@ class MainActivity : AppCompatActivity() {
 
     //get data from Racine_des_mots_arabe.csv
     //zsourat  is in format 1:1 aka sourat:aya
-    open fun ReadDatafromcsvMot_des_racines(IdRacine: String?): ElementscsvMotsdesRacines {
-        val tub = ElementscsvMotsdesRacines()
+    open fun ReadDatafromcsvMot_des_racines(IdRacine: String?): MutableList<ElementscsvMotsdesRacines>? {
+        var tub = ElementscsvMotsdesRacines()
+        var tub2 = ElementscsvMotsdesRacines()
+
+        var ayat: MutableList<ElementscsvMotsdesRacines>? = mutableListOf(tub)// = arrayOf<ElementscsvMotsdesRacines>()
+
         val `is`: InputStream = resources.openRawResource(R.raw.motsdesracines1)
         val reader: BufferedReader = BufferedReader(
             InputStreamReader(`is`, Charset.forName("UTF-8"))
@@ -137,21 +178,31 @@ class MainActivity : AppCompatActivity() {
                 //Log.d("token3",tokens[3].toString())
                 //Log.d("di",sourat)
                 if (tokens[2]==IdRacine) { //search in column Z
+                        val tub2 = ElementscsvMotsdesRacines()
 
-                    tub.ID_Aya=tokens[1]
-                    tub.ID_Word=tokens[0]
 
-                    tub.idRacine=tokens[2]
-                    tub.englishWord=tokens[3]
-                    break
+                        tub2.ID_Aya=tokens[1]
+                        tub2.ID_Word=tokens[0]
+
+                        tub2.idRacine=tokens[2]
+                        tub2.englishWord=tokens[3]
+
+                        ayat?.add(tub2)
+
+
+
                 }
                 //line= reader.readLine()
                 //if (line==null) break
+
+                if(tokens[0]=="77879"){
+                    break
+                }
             }
         } catch (e: IOException) {
             e.printStackTrace()
         }
-        return tub
+        return ayat
     }
 
 
