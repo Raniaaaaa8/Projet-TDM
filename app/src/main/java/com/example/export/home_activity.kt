e5b1a156e7315775ff13de802d0fc2081e547b16
Raniaaaaa8 <hr_bouguesri@esi.dz>
2021-07-07 21:50:12 +0100

    interface change and display of all racines

--- app/src/main/java/com/example/export/home_activity.kt
+++ app/src/main/java/com/example/export/home_activity.kt
@@ -1,42 +1,91 @@
 package com.example.export
 
-
-import android.annotation.SuppressLint
-import android.content.Intent
-import android.os.Bundle
-import android.os.Handler
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main2.*
+import android.os.Bundle
+import android.util.Log
+import android.widget.SearchView
+import kotlinx.android.synthetic.main.activity_home.*
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStream
 import java.io.InputStreamReader
 import java.nio.charset.Charset
 
-
-class MainActivity : AppCompatActivity() {
-    @SuppressLint("SetTextI18n")
+class home_activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        supportActionBar?.hide()
-        Handler().postDelayed({
-            val intent = Intent(this@MainActivity, home_activity::class.java)
-            startActivity(intent)
-            finish()
-        }, 3000)
-
+        setContentView(R.layout.activity_home)
+        affichage()
+        val str=ReadDataAllcsvRacine_des_mots_arabe()
+        Log.d("str racine",str)
+        text_view_result.setText(str)
     }
 
 
-    }
 
-   /* fun affichage(){
+
+    fun affichage(){
 
         var sourat:String?=null
         var racine:String?=null
-        button.setOnClickListener {
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextSubmit(query: String): Boolean {
+                // task HERE
+                //recuperer le input aka la racine a cherché
+                racine=searchView.query.toString()
+                Log.d("search", racine!!)
+
+                //rechercher le Id racine dans Racine des mots
+                var ele=ReadDatafromcsvRacine_des_mots_arabe(racine)
+                if (ele!=null) {
+                    //afficher les aya
+                        //recherche dans Mot des quran les aya ayant le id_racine trouvé
+                    var ayat= ReadDatafromcsvMot_des_racines(ele.idRacine)//mutableListOf(idaya)// = arrayOf<ElementscsvMotsdesRacines>()
+
+
+                    var str= ayat?.get(0)?.englishWord
+                    if(str==null) str=""
+                    Log.d("array aya",str)
+                    str=""
+                    var f:String?
+                    if (ayat != null) {
+                        var cpt = 1
+                        for (i in ayat) {
+                            if (cpt == ayat.size) break
+                            f = ReadDatafromcsvAya(ayat?.get(cpt)?.ID_Aya).Text_AR
+                            if (f != null) str = str + "  ***  " + f
+
+                            Log.d("cpt", cpt.toString())
+                            var str2 = ayat?.get(cpt)?.ID_Aya
+                            if (str2 != null)
+                                Log.d("ayat", str2)
+                            if (f != null) {
+                                Log.d("f", f)
+                            }
+                            cpt++
+                        }
+                    }
+                    if (str!=null) Log.d("str",str)
+
+                }
+                else Log.d("erreur ele", "idracine pas trouvé")
+
+
+
+                return false
+            }
+
+        })
+
+
+
+        /*button.setOnClickListener {
             sourat= mot.text.toString() //get from input
             var tub=ReadDatafromcsvAya(sourat)//search
             if (tub!=null) aya.setText(tub.Text_AR)//display
@@ -82,14 +131,14 @@ class MainActivity : AppCompatActivity() {
             else displayIdRacine.setText("Not found")
 
 
-        }
+        }*/
     }
 
 
 
     //get data from aya.csv
     //zsourat  is in format 1:1 aka sourat:aya
-    fun ReadDatafromcsvAya(zsourat: String?): ElementscsvAya {
+    open fun ReadDatafromcsvAya(zsourat: String?): ElementscsvAya {
         val tub = ElementscsvAya()
         val `is`: InputStream = resources.openRawResource(R.raw.aya)
         val reader: BufferedReader = BufferedReader(
@@ -194,16 +243,16 @@ class MainActivity : AppCompatActivity() {
                 //Log.d("token3",tokens[3].toString())
                 //Log.d("di",sourat)
                 if (tokens[2]==IdRacine) { //search in column Z
-                        val tub2 = ElementscsvMotsdesRacines()
+                    val tub2 = ElementscsvMotsdesRacines()
 
 
-                        tub2.ID_Aya=tokens[1]
-                        tub2.ID_Word=tokens[0]
+                    tub2.ID_Aya=tokens[1]
+                    tub2.ID_Word=tokens[0]
 
-                        tub2.idRacine=tokens[2]
-                        tub2.englishWord=tokens[3]
+                    tub2.idRacine=tokens[2]
+                    tub2.englishWord=tokens[3]
 
-                        ayat?.add(tub2)
+                    ayat?.add(tub2)
 
 
 
@@ -219,7 +268,52 @@ class MainActivity : AppCompatActivity() {
             e.printStackTrace()
         }
         return ayat
-    }*/
+    }
 
+
+    open fun ReadDataAllcsvRacine_des_mots_arabe(): String {
+
+        var str:String=""
+        var ayat: MutableList<ElementscsvRacine_des_mots_arabe>? =null//= mutableListOf(tub)// = arrayOf<ElementscsvMotsdesRacines>()
+
+        val `is`: InputStream = resources.openRawResource(R.raw.racinedesmotsarabe)
+        val reader: BufferedReader = BufferedReader(
+            InputStreamReader(`is`, Charset.forName("UTF-8"))
+        )
+        var line = ""
+        try {
+
+            reader.readLine()
+
+            while (reader.readLine().also { line=it } != null ) {
+
+                val tokens = line.split(",").toTypedArray()
+                //read data
+                var tub = ElementscsvRacine_des_mots_arabe()
+
+                //Log.d("token3",tokens[3].toString())
+                //Log.d("di",sourat)
+               // if (tokens[1]==Racine) { //search in column Z
+
+                    tub.Racine=tokens[1]
+                    tub.idRacine=tokens[0]
+                    tub.NBLettre=tokens[7]
+                    str=str+"\n"+tub.Racine
+              //  }
+                if(tokens[0]=="1799"){
+
+                    break
+                }
+                //line= reader.readLine()
+                //if (line==null) break
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        return str
+    }
+
+}
 
 
